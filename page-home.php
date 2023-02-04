9eb2fbdaa8ff1594c3d2e8c9bedeaf74753e1da7
Fox Scarlett <foxontheone@gmail.com>
2023-02-04 00:28:41 -0800

    home page

---
+++ page-home.php
@@ -0,0 +1,57 @@
+
+
+<?php
+/**
+ * Template Name: Home Page
+ *
+ * @package Sandbox
+ */
+
+get_header();
+?>
+
+	<main id="primary" class="site-main">
+		<div class="sandbox-container">
+
+
+
+			<?php
+			while ( have_posts() ) :
+				the_post();
+
+				?>
+
+				<section class="sandbox-main-heading-wrapper">
+					<h1 class="sandbox-home-heading-1">Sandbox</h1>
+				</section>
+
+
+				<section class="sandbox-home-blurb-wrapper">
+					
+				</section>
+
+
+				<section class="sandbox-home-image-section">
+					<div class="sandbox-image-wrapper">image here</div>
+				</section>
+
+
+				
+
+
+
+					
+					<?php  the_content(); ?>
+
+
+				<?php
+
+			endwhile; // End of the loop.
+			?>
+		</div>
+
+	</main><!-- #main -->
+
+<?php
+// get_sidebar();
+get_footer();
