c3a91ed40f2862965539363fd8b052e6cd6e985d
Fox Scarlett <foxontheone@gmail.com>
2023-02-05 15:50:22 -0800

    single blog

--- single.php
+++ single.php
@@ -10,31 +10,42 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+<div class="blog-wrapper">
+	<main id="primary" class="site-main sandbox-single-blog-container">
+		<div class="">
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+			<?php
+			while ( have_posts() ) :
+				the_post();
 
-			get_template_part( 'template-parts/content', get_post_type() );
+				get_template_part( 'template-parts/content', get_post_type() );
 
-			the_post_navigation(
-				array(
-					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'sandbox' ) . '</span> <span class="nav-title">%title</span>',
-					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'sandbox' ) . '</span> <span class="nav-title">%title</span>',
-				)
-			);
+				the_post_navigation(
+					array(
+						'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'sandbox' ) . '</span> <span class="nav-title">%title</span>',
+						'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'sandbox' ) . '</span> <span class="nav-title">%title</span>',
+					)
+				);
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+				// If comments are open or we have at least one comment, load up the comment template.
+				// if ( comments_open() || get_comments_number() ) :
+				// 	comments_template();
+				// endif;
 
-		endwhile; // End of the loop.
-		?>
+			endwhile; // End of the loop.
+			?>
+
+		</div>
 
 	</main><!-- #main -->
 
+	<aside id="sidebar" class="sandbox-blog-sidebar">
+		<?php
+		get_sidebar();
+
+		?>
+	</aside>
+</div>
+
 <?php
-get_sidebar();
 get_footer();
