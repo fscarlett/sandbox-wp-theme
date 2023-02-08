475d234256656f4ff0c7e9bd5c2ceb3ede75db42
Fox Scarlett <foxontheone@gmail.com>
2023-02-08 00:01:08 -0800

    team post type

--- inc/post-types.php
+++ inc/post-types.php
@@ -79,6 +79,44 @@ function sandbox_post_types() {
 
   register_post_type( 'sandbox_testimonial', $testimonial_args );
 
+  //register Team post type
+  $team_labels = array(
+    'name'                => 'Team Members',
+    'singular_name'       => 'Team Member',
+    'menu_name'           => 'Team Members',
+    'name_admin_bar'      => 'Team Member',
+    'add_new'             => 'Add New',
+    'add_new_item'        => 'Add New Team Member',
+    'new_item'            => 'New Team Member',
+    'edit_item'           => 'Edit Team Member',
+    'view_item'           => 'View Team Member',
+    'all_items'           => 'All Team Members',
+    'search_items'        => 'Search For Team Members',
+    'parent_item_colon'   => 'Parent Team Members:',
+    'not_found'           => 'No Team Member found.',
+    'not_found_in_trash'  => 'No Team Member found in trash.',
+    );
+  $team_args = array(
+    'labels'              => $team_labels,
+    'description'         => 'team',
+    'public'              => true,
+    'publicly_queryable'  => true,
+    'show_ui'             => true,
+    'query_var'           => true,
+    'rewrite'             => array( 'slug' => 'team' ),
+    'capability_type'     => 'post',
+    'map_meta_cap'        => true,
+    'exclude_from_search' => false,
+    'has_archive'         => false,
+    'hierarchical'        => false,
+    'taxonomies'          => array( 'category' ),
+    'menu_position'       => null,
+    'supports'            => array( 'title', 'editor', 'excerpt', 'page-attributes' ),
+    'menu_icon'           => 'dashicons-groups',
+    );
+
+  register_post_type( 'sandbox_team', $team_args );
+
 
 
 }
