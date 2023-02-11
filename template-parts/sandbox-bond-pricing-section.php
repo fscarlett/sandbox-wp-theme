23282bf5bb0c3093e4938eb1937b32f2152f93e3
Fox Scarlett <foxontheone@gmail.com>
2023-02-10 22:23:03 -0800

    bond page template & pricing template part

---
+++ template-parts/sandbox-bond-pricing-section.php
@@ -0,0 +1,19 @@
+<section id="pricing" class="sandbox-bond-pricing-section">
+
+	<h2>Pricing Plans</h2>
+	<div class="sandbox-pricing-cards-wrapper">
+
+		<div class="sandbox-pricing-card">hello</div>
+		<div class="sandbox-pricing-card">hello</div>
+		<div class="sandbox-pricing-card">hello</div>
+		<div class="sandbox-pricing-card">hello</div>
+		
+	</div>
+	<div class="sandbox-pricing-enterprise-wrapper">
+		enterprise
+	</div>
+
+	
+
+
+</section>
