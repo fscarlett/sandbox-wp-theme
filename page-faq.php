f4cf6ce10a52fa5f0f8ee83df11b5791a4f55153
Fox Scarlett <foxontheone@gmail.com>
2023-06-15 22:32:46 -0700

    faq page add summary page qs loop

--- page-faq.php
+++ page-faq.php
@@ -78,7 +78,39 @@ get_header();
           endif;
         ?>
 
-      </div>   
+      </div> 
+      
+      <div class="sandbox-faqs-wrapper">
+        <h2>Summary Page Questions</h2>
+        <?php
+
+          if( have_rows('sandbox_faq_summary') ):
+
+              while( have_rows('sandbox_faq_summary') ) : the_row();
+
+                  $sbox_question_summary = get_sub_field('faq_question_summary');
+                  $sbox_answer_summary = get_sub_field('faq_answer_summary');
+              ?>
+
+              <div class="sandbox-faq-card">
+                <h3><?php echo $sbox_question_summary; ?> </h3>
+                <p><?php echo $sbox_answer_summary; ?> </p>
+
+
+              </div>
+
+              <?php
+              endwhile;
+
+          else :
+            ?>
+              <h3>No FAQs were found.</h3>
+              <?php
+
+          endif;
+        ?>
+
+      </div> 
 
     </div>
 
