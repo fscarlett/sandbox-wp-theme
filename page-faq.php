07d4a7eab020aac39b445e779b04c74f67c39c27
Fox Scarlett <foxontheone@gmail.com>
2023-06-15 22:04:19 -0700

    faq page switch unspecified acf field names to general qs

--- page-faq.php
+++ page-faq.php
@@ -19,17 +19,17 @@ get_header();
       <div class="sandbox-faqs-wrapper">
         <?php
 
-          if( have_rows('sandbox_faq') ):
+          if( have_rows('sandbox_faq_general') ):
 
-              while( have_rows('sandbox_faq') ) : the_row();
+              while( have_rows('sandbox_faq_general') ) : the_row();
 
-                  $sbox_question = get_sub_field('faq_question');
-                  $sbox_answer = get_sub_field('faq_answer');
+                  $sbox_question_general = get_sub_field('faq_question_general');
+                  $sbox_answer_general = get_sub_field('faq_answer_general');
               ?>
 
               <div class="sandbox-faq-card">
-                <h3><?php echo $sbox_question; ?> </h3>
-                <p><?php echo $sbox_answer; ?> </p>
+                <h3><?php echo $sbox_question_general; ?> </h3>
+                <p><?php echo $sbox_answer_general; ?> </p>
 
 
               </div>
