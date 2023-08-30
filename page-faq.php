2dd20eeeab5d330e26241c20dee1fbb9fae0bf2d
Fox Scarlett <foxontheone@gmail.com>
2023-08-29 23:22:52 -0700

    FAQ page implement html details/summary

--- page-faq.php
+++ page-faq.php
@@ -20,18 +20,23 @@ get_header();
         <h2>General Questions</h2>
         <?php
 
+          $sandbox_row_number = 0;
+
           if( have_rows('sandbox_faq_general') ):
 
               while( have_rows('sandbox_faq_general') ) : the_row();
 
                   $sbox_question_general = get_sub_field('faq_question_general');
                   $sbox_answer_general = get_sub_field('faq_answer_general');
+                  $sandbox_row_number++;
               ?>
 
               <div class="sandbox-faq-card">
-                <h3><?php echo $sbox_question_general; ?> </h3>
+              <details>
+                 <summary><span> <?php echo sprintf('%03d', $sandbox_row_number); ?> </span>
+                <h3><?php echo $sbox_question_general; ?> </h3></summary>
                 <p><?php echo $sbox_answer_general; ?> </p>
-
+              </details>
 
               </div>
 
@@ -51,6 +56,7 @@ get_header();
       <div class="sandbox-faqs-wrapper">
         <h2>Data Questions</h2>
         <?php
+        $sandbox_row_number = 0;
 
           if( have_rows('sandbox_faq_data') ):
 
@@ -58,11 +64,15 @@ get_header();
 
                   $sbox_question_data = get_sub_field('faq_question_data');
                   $sbox_answer_data = get_sub_field('faq_answer_data');
+                  $sandbox_row_number++;
               ?>
 
               <div class="sandbox-faq-card">
-                <h3><?php echo $sbox_question_data; ?> </h3>
+              <details>
+                 <summary><span> <?php echo sprintf('%03d', $sandbox_row_number); ?> </span>
+                <h3><?php echo $sbox_question_data; ?> </h3></summary>
                 <p><?php echo $sbox_answer_data; ?> </p>
+              </details>
 
 
               </div>
@@ -83,6 +93,7 @@ get_header();
       <div class="sandbox-faqs-wrapper">
         <h2>Summary Page Questions</h2>
         <?php
+        $sandbox_row_number = 0;
 
           if( have_rows('sandbox_faq_summary') ):
 
@@ -90,12 +101,15 @@ get_header();
 
                   $sbox_question_summary = get_sub_field('faq_question_summary');
                   $sbox_answer_summary = get_sub_field('faq_answer_summary');
+                  $sandbox_row_number++;
               ?>
 
               <div class="sandbox-faq-card">
-                <h3><?php echo $sbox_question_summary; ?> </h3>
+              <details>
+                 <summary><span> <?php echo sprintf('%03d', $sandbox_row_number); ?> </span>
+                <h3><?php echo $sbox_question_summary; ?> </h3></summary>
                 <p><?php echo $sbox_answer_summary; ?> </p>
-
+              </details>
 
               </div>
 
@@ -115,6 +129,7 @@ get_header();
       <div class="sandbox-faqs-wrapper">
         <h2>Reports Page Questions</h2>
         <?php
+        $sandbox_row_number = 0;
 
           if( have_rows('sandbox_faq_reports') ):
 
@@ -122,12 +137,15 @@ get_header();
 
                   $sbox_question_reports = get_sub_field('faq_question_reports');
                   $sbox_answer_reports = get_sub_field('faq_answer_reports');
+                  $sandbox_row_number++;
               ?>
 
               <div class="sandbox-faq-card">
-                <h3><?php echo $sbox_question_reports; ?> </h3>
+              <details>
+                 <summary><span> <?php echo sprintf('%03d', $sandbox_row_number); ?> </span>
+                <h3><?php echo $sbox_question_reports; ?> </h3></summary>
                 <p><?php echo $sbox_answer_reports; ?> </p>
-
+              </details>
 
               </div>
 
@@ -147,6 +165,7 @@ get_header();
       <div class="sandbox-faqs-wrapper">
         <h2>Inputs Page Questions</h2>
         <?php
+        $sandbox_row_number = 0;
 
           if( have_rows('sandbox_faq_inputs') ):
 
@@ -154,12 +173,15 @@ get_header();
 
                   $sbox_question_inputs = get_sub_field('faq_question_inputs');
                   $sbox_answer_inputs = get_sub_field('faq_answer_inputs');
+                  $sandbox_row_number++;
               ?>
 
               <div class="sandbox-faq-card">
-                <h3><?php echo $sbox_question_inputs; ?> </h3>
+              <details>
+                 <summary><span> <?php echo sprintf('%03d', $sandbox_row_number); ?> </span>
+                <h3><?php echo $sbox_question_inputs; ?> </h3></summary>
                 <div class="sandbox-faq-card-wysiwyg"><?php echo $sbox_answer_inputs; ?> </div>
-
+              </details>
 
               </div>
 
