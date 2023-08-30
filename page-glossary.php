c7a19ccc58f07d69cf2504de16cfb2dcc3045edd
Fox Scarlett <foxontheone@gmail.com>
2023-08-29 23:08:23 -0700

    glossary add line numbers

--- page-glossary.php
+++ page-glossary.php
@@ -19,17 +19,21 @@ get_header();
       <div class="sandbox-faqs-wrapper">
         <?php
 
+        $sandbox_row_number = 0;
+
           if( have_rows('sandbox_glossary_item') ):
 
               while( have_rows('sandbox_glossary_item') ) : the_row();
 
                   $sbox_question_general = get_sub_field('glossary_word');
                   $sbox_answer_general = get_sub_field('glossary_definition');
+                  $sandbox_row_number++;
               ?>
 
               <div class="sandbox-faq-card">
+             
                 <details>
-                 <summary><h3><?php echo $sbox_question_general; ?> </h3></summary>
+                 <summary><span> <?php echo sprintf('%03d', $sandbox_row_number); ?> </span><h3><?php echo $sbox_question_general; ?> </h3></summary>
                   <p><?php echo $sbox_answer_general; ?> </p>
                 </details>
 
