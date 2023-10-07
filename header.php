a03fdea2f8f2a30a9687e73a5147781e19116ce0
Fox Scarlett <foxontheone@gmail.com>
2023-10-06 20:46:52 -0700

    install hotjar tracking code in head

--- header.php
+++ header.php
@@ -17,6 +17,18 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
+	<!-- Hotjar Tracking Code for https://sandboxc6.com/ -->
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:2958762,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
+
 	<?php wp_head(); ?>
 </head>
 
