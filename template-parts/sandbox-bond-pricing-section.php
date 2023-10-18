4e5f1b47de779d5fda4ca797c0fdcc6f3334c4c9
Fox Scarlett <foxontheone@gmail.com>
2023-10-18 13:31:04 -0700

    remove new pricing banner

--- template-parts/sandbox-bond-pricing-section.php
+++ template-parts/sandbox-bond-pricing-section.php
@@ -4,12 +4,12 @@
 
 	<h2>Pricing Plans</h2>
 
-	<div class="sandbox-pricing-cards-wrapper">
-	<div class="sandbox-pricing-banner">
-		<h3 class="sandbox-pricing-banner-heading">New Bond Pricing! Only $9.99 per month!</h3>
-		<p>Thank you to all of our users for helping us hit our critical user milestone, allowing us to offer reduced rates to all users, old and new.</p>
-	</div>
-</div>
+	<!-- <div class="sandbox-pricing-cards-wrapper">
+		<div class="sandbox-pricing-banner">
+			<h3 class="sandbox-pricing-banner-heading">New Bond Pricing! Only $9.99 per month!</h3>
+			<p>Thank you to all of our users for helping us hit our critical user milestone, allowing us to offer reduced rates to all users, old and new.</p>
+		</div>
+	</div> -->
 	<div class="sandbox-pricing-cards-wrapper">
 
 		<div class="sandbox-pricing-card">
