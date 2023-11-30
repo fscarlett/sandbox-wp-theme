8685f9f142b2d1baf99821232511afca77765b33
Fox Scarlett <foxontheone@gmail.com>
2023-11-29 20:08:29 -0800

    fix insta section styles

--- template-parts/sandbox-insta-section.php
+++ template-parts/sandbox-insta-section.php
@@ -1,5 +1,5 @@
 <section class="sandbox-insta-section" id="sandbox-insta-section">
-  <div class="sandbox-container">
+  <div class="sandbox-insta-container">
     <h2>Follow Us On Instagram</h2>
     <?php echo do_shortcode('[instagram-feed feed=1]'); ?>
   </div>
