bd8b7c96f8cf788c4b2bb871a7e0448bd4872758
Fox Scarlett <foxontheone@gmail.com>
2023-11-29 19:27:59 -0800

    create partial insta section

---
+++ template-parts/sandbox-insta-section.php
@@ -0,0 +1,6 @@
+<section class="sandbox-insta-section" id="sandbox-insta-section">
+  <div class="sandbox-container">
+    <h2>Follow Us On Instagram</h2>
+    <?php echo do_shortcode('[instagram-feed feed=1]'); ?>
+  </div>
+</section>
