74c51528f03d47acaa5fa31efa890a45f9b87679
Fox Scarlett <foxontheone@gmail.com>
2025-07-22 20:07:21 -0700

    add Mawacalc landing page template, separate footer

---
+++ page-mawacalc.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Template Name: MAWACalc Page
+ *
+ * @package Sandbox
+ */
+
+get_header();
+?>
+
+	<main id="primary" class="site-main">
+		<div class="sandbox-container">
+
+
+			<?php
+			while ( have_posts() ) :
+				the_post();
+
+				get_template_part( 'template-parts/content', 'page' );
+
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+
+			endwhile; // End of the loop.
+			?>
+		</div>
+
+	</main><!-- #main -->
+
+<?php
+// get_sidebar();
+get_footer('landingpage');
